8543eaa2bd9377d0e6304604d02d8f89591d5dcb
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2022-03-05 15:51:17 +0100

    Sort matches by score

--- search-server/src/main.rs
+++ search-server/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, collections::HashMap};
 
 use serde::Serialize;
 
@@ -31,25 +31,32 @@ fn main() {
                 .filter(|w| w.len() > 2)
                 .map(|w| w.to_owned())
                 .collect();
-            let mut results: Vec<QueryResponseItem> = Vec::new();
-            'search: for w in words.iter() {
+            let mut score_by_result_index: HashMap<u32, f32> = HashMap::new();
+            for w in words.iter() {
                 if let Some(matches) = search_index.words.get(w) {
                     for m in matches {
-                        let result = &search_index.results[m.result_index as usize];
-                        let image_id = &search_index.image_ids[result.image_index as usize];
-                        results.push(QueryResponseItem {
-                            image_id: image_id.clone(),
-                            x: result.x,
-                            y: result.y,
-                            width: result.width,
-                            height: result.height,
-                        });
-                        if results.len() > 5 {
-                            break 'search;
-                        }
+                        *score_by_result_index
+                            .entry(m.result_index)
+                            .or_default() += m.score;
                     }
                 }
             }
+            let mut sorted: Vec<_> = score_by_result_index.into_iter().collect();
+            sorted.sort_unstable_by(|(_, s_a), (_, s_b)| s_b.partial_cmp(s_a).unwrap());
+            let results: Vec<QueryResponseItem> = sorted.iter()
+                .map(|(r, _)| {
+                   let result = &search_index.results[*r as usize];
+                    let image_id = &search_index.image_ids[result.image_index as usize];
+                    QueryResponseItem {
+                        image_id: image_id.clone(),
+                        x: result.x,
+                        y: result.y,
+                        width: result.width,
+                        height: result.height,
+                    }
+                })
+                .take(5)
+                .collect();
             let body = serde_json::to_string(&results).unwrap().into();
             Response {
                 status_code: 200,
