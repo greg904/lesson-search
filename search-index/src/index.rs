93fbcd079ba663ce5a86067193e6ba8cf6fc4b49
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2022-03-07 20:49:03 +0100

    search-index-generator: Add test to check for results

---
+++ search-index/src/index.rs
@@ -0,0 +1,211 @@
+use std::{
+    collections::HashMap,
+    io::{self, Read, Write},
+};
+
+fn deserialize_u32<R: Read>(r: &mut R) -> io::Result<u32> {
+    let mut buf = [0u8; 4];
+    r.read_exact(&mut buf)?;
+    Ok(u32::from_le_bytes(buf))
+}
+
+fn deserialize_f32<R: Read>(r: &mut R) -> io::Result<f32> {
+    let mut buf = [0u8; 4];
+    r.read_exact(&mut buf)?;
+    Ok(f32::from_le_bytes(buf))
+}
+
+fn deserialize_u16<R: Read>(r: &mut R) -> io::Result<u16> {
+    let mut buf = [0u8; 2];
+    r.read_exact(&mut buf)?;
+    Ok(u16::from_le_bytes(buf))
+}
+
+fn deserialize_i16<R: Read>(r: &mut R) -> io::Result<i16> {
+    let mut buf = [0u8; 2];
+    r.read_exact(&mut buf)?;
+    Ok(i16::from_le_bytes(buf))
+}
+
+fn deserialize_string<R: Read>(r: &mut R) -> io::Result<String> {
+    let len = deserialize_u32(r)?;
+
+    let mut s = vec![0u8; len as usize];
+    r.read_exact(&mut s)?;
+
+    String::from_utf8(s).map_err(|_err| io::Error::new(io::ErrorKind::InvalidData, "not UTF-8"))
+}
+
+fn deserialize_vec_string<R: Read>(r: &mut R) -> io::Result<Vec<String>> {
+    let count = deserialize_u32(r)?;
+
+    let mut vec = Vec::with_capacity(count as usize);
+    for _ in 0..count {
+        vec.push(deserialize_string(r)?);
+    }
+
+    Ok(vec)
+}
+
+#[derive(Clone)]
+pub struct Page {
+    pub document_index: u16,
+    pub page_nr: u16,
+    pub rendered_image_id: String,
+    pub width: u16,
+    pub height: u16,
+}
+
+impl Page {
+    fn deserialize<R: Read>(r: &mut R) -> io::Result<Self> {
+        let document_index = deserialize_u16(r)?;
+        let page_nr = deserialize_u16(r)?;
+        let rendered_image_id = deserialize_string(r)?;
+        let width = deserialize_u16(r)?;
+        let height = deserialize_u16(r)?;
+
+        Ok(Self {
+            document_index,
+            page_nr,
+            rendered_image_id,
+            width,
+            height,
+        })
+    }
+}
+
+#[derive(Clone)]
+pub struct SearchResult {
+    pub page_index: u32,
+    pub x: i16,
+    pub y: i16,
+    pub width: u16,
+    pub height: u16,
+}
+
+impl SearchResult {
+    fn deserialize<R: Read>(r: &mut R) -> io::Result<SearchResult> {
+        let page_index = deserialize_u32(r)?;
+        let x = deserialize_i16(r)?;
+        let y = deserialize_i16(r)?;
+        let width = deserialize_u16(r)?;
+        let height = deserialize_u16(r)?;
+
+        Ok(SearchResult {
+            page_index,
+            x,
+            y,
+            width,
+            height,
+        })
+    }
+}
+
+#[derive(Clone)]
+pub struct Match {
+    pub result_index: u32,
+    pub score: f32,
+}
+
+impl Match {
+    fn deserialize<R: Read>(r: &mut R) -> io::Result<Match> {
+        let result_index = deserialize_u32(r)?;
+        let score = deserialize_f32(r)?;
+
+        Ok(Match {
+            result_index,
+            score,
+        })
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct SearchIndex {
+    pub documents: Vec<String>,
+    pub pages: Vec<Page>,
+    pub results: Vec<SearchResult>,
+    pub words: HashMap<String, Vec<Match>>,
+}
+
+impl SearchIndex {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn deserialize<R: Read>(r: &mut R) -> io::Result<Self> {
+        let documents = deserialize_vec_string(r)?;
+
+        let page_count = deserialize_u32(r)?;
+        let mut pages = Vec::with_capacity(page_count as usize);
+        for _ in 0..page_count {
+            pages.push(Page::deserialize(r)?);
+        }
+
+        let result_count = deserialize_u32(r)?;
+        let mut results = Vec::with_capacity(result_count as usize);
+        for _ in 0..result_count {
+            results.push(SearchResult::deserialize(r)?);
+        }
+
+        let word_count = deserialize_u32(r)?;
+        let mut words = HashMap::with_capacity(word_count as usize);
+        for _ in 0..word_count {
+            let word = deserialize_string(r)?;
+
+            let match_count = deserialize_u32(r)?;
+            let mut matches = Vec::with_capacity(match_count as usize);
+            for _ in 0..match_count {
+                matches.push(Match::deserialize(r)?);
+            }
+
+            words.insert(word, matches);
+        }
+
+        Ok(Self {
+            documents,
+            pages,
+            results,
+            words,
+        })
+    }
+
+    pub fn serialize<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        w.write_all(&(self.documents.len() as u32).to_le_bytes())?;
+        for doc in self.documents.iter() {
+            w.write_all(&(doc.len() as u32).to_le_bytes())?;
+            w.write_all(doc.as_bytes())?;
+        }
+
+        w.write_all(&(self.pages.len() as u32).to_le_bytes())?;
+        for page in self.pages.iter() {
+            w.write_all(&page.document_index.to_le_bytes())?;
+            w.write_all(&page.page_nr.to_le_bytes())?;
+            w.write_all(&(page.rendered_image_id.len() as u32).to_le_bytes())?;
+            w.write_all(page.rendered_image_id.as_bytes())?;
+            w.write_all(&page.width.to_le_bytes())?;
+            w.write_all(&page.height.to_le_bytes())?;
+        }
+
+        w.write_all(&(self.results.len() as u32).to_le_bytes())?;
+        for r in self.results.iter() {
+            w.write_all(&r.page_index.to_le_bytes())?;
+            w.write_all(&r.x.to_le_bytes())?;
+            w.write_all(&r.y.to_le_bytes())?;
+            w.write_all(&r.width.to_le_bytes())?;
+            w.write_all(&r.height.to_le_bytes())?;
+        }
+
+        w.write_all(&(self.words.len() as u32).to_le_bytes())?;
+        for (word, matches) in self.words.iter() {
+            w.write_all(&(word.len() as u32).to_le_bytes())?;
+            w.write_all(word.as_bytes())?;
+            w.write_all(&(matches.len() as u32).to_le_bytes())?;
+            for m in matches.iter() {
+                w.write_all(&m.result_index.to_le_bytes())?;
+                w.write_all(&m.score.to_le_bytes())?;
+            }
+        }
+
+        Ok(())
+    }
+}
--- search-server/src/main.rs
+++ search-server/src/main.rs
@@ -4,10 +4,10 @@ use std::{
     fs::File,
 };
 
+use search_index::index::SearchIndex;
 use serde::Serialize;
 
 use http_server::{HttpServer, Response};
-use search_index::SearchIndex;
 
 mod http_server;
 
@@ -44,68 +44,22 @@ fn main() {
     server
         .serve(|req| {
             let query = urlencoding::decode(&req.url[1..]).unwrap();
-            let normalized = search_index::normalize(&query);
-            let pages = if normalized.is_empty() {
-                Vec::new()
-            } else {
-                let words: Vec<String> = search_index::normalize(&query)
-                    .split(' ')
-                    .map(|w| w.to_owned())
-                    .collect();
-                let mut pages: BTreeMap<u32, (Vec<u32>, HashMap<String, f32>)> = BTreeMap::new();
-                for w in words.iter() {
-                    if let Some(matches) = search_index.words.get(w) {
-                        for m in matches {
-                            let result = &search_index.results[m.result_index as usize];
-                            let (results, max_scores) = pages.entry(result.page_index).or_default();
-                            // Limit the amount of rect per page.
-                            if results.len() >= 20 {
-                                continue;
-                            }
-                            results.push(m.result_index);
-                            let max_score = max_scores.entry(w.to_owned()).or_default();
-                            *max_score = max_score.max(m.score);
-                        }
-                    }
-                }
-
-                let mut pages: Vec<_> = pages.into_iter().collect();
-                pages.sort_by(|(_, page_a), (_, page_b)| {
-                    let score_a: f32 = page_a.1.values().sum();
-                    let score_b: f32 = page_b.1.values().sum();
-                    score_b.partial_cmp(&score_a).unwrap()
-                });
-
-                pages
-                    .into_iter()
-                    .map(|(page_index, page_search)| {
-                        let (result_indices, _score) = page_search;
-                        let rects = result_indices
-                            .into_iter()
-                            .map(|r| {
-                                let result = &search_index.results[r as usize];
-                                Rect {
-                                    x: result.x,
-                                    y: result.y,
-                                    width: result.width,
-                                    height: result.height,
-                                }
-                            })
-                            .collect();
-                        let page = &search_index.pages[page_index as usize];
-                        let document_name = &search_index.documents[page.document_index as usize];
-                        Page {
-                            document_name: document_name.to_owned(),
-                            page_nr: page.page_nr,
-                            rendered_image_id: page.rendered_image_id.clone(),
-                            width: page.width,
-                            height: page.height,
-                            rects,
-                        }
-                    })
-                    .take(10)
-                    .collect()
-            };
+            let pages: Vec<_> = search_index::search::search(&search_index, &query)
+                .into_iter()
+                .map(|p| Page {
+                    document_name: p.document_digest,
+                    page_nr: p.number,
+                    rendered_image_id: p.image_digest,
+                    width: p.width,
+                    height: p.height,
+                    rects: p.highlights.into_iter().map(|h| Rect {
+                        x: h.x,
+                        y: h.y,
+                        width: h.width,
+                        height: h.height,
+                    }).collect(),
+                })
+                .collect();
             let body: Vec<u8> = serde_json::to_string(&pages).unwrap().into();
             let mut headers = vec![("Content-Length".to_string(), body.len().to_string())];
             if !cors_origin.is_empty() {
